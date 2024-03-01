61db6d8256ec37f4dcb766a88cba4bc2bac58378
Sathyanath-R <161233995+Sathyanath-R@users.noreply.github.com>
2024-03-01 11:40:21 +0530

    Update YTDH.py

--- YTDH.py
+++ YTDH.py
@@ -8,26 +8,35 @@ import mysql.connector
 import json
 import mysql.connector
 import streamlit as st
+import pandas as pd
 
-chID = st.text_input("Enter Channel Id")
-#chID="UCduIoIMfD8tT3KoU0-zBRgQ"
-apiKey="AIzaSyCavZTVfOycz7mRQG_8Px-_cd6h7GHpf-k"
+# Function to connect Youtube API via. google API client
+def google_api_config():
+    apiKey="AIzaSyCavZTVfOycz7mRQG_8Px-_cd6h7GHpf-k"
+    api_service_name = "youtube"
+    api_version = "v3"
+    youtube_api_connect = googleapiclient.discovery.build(api_service_name, api_version, developerKey=apiKey)
+    return youtube_api_connect
 
-api_service_name = "youtube"
-api_version = "v3"
+youtube = google_api_config()
 
+# MongoDB Connection
 uri = "mongodb://localhost:27017/"
-
 client = pymongo.MongoClient(uri)
 db = client["youtubedb"]
 channels_collection = db["channels"]
 
-#channel_video_comment_dict = {}
-#v1 = None
-#@st.cache(suppress_st_warning=True)
+# MySQL Connection
+mysql_config = {
+    'user':'dev_user', 'password':'My$ql123',
+    'host':'127.0.0.1', 'database':'youtubedb'
+    }
 
+mysql_connect = mysql.connector.connect(**mysql_config)
 
+mysql_cursor = mysql_connect.cursor()
 
+#Function to extract given Youtube Channel Data
 def extract_channel_data(Channel_ID):
 
     try:
@@ -36,20 +45,15 @@ def extract_channel_data(Channel_ID):
 
         channel_video_comment_dict ={}
 
-        youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, developerKey=apiKey)
-
         request = youtube.channels().list(
         part="snippet,contentDetails,statistics",
-        id=chID
+        id=Channel_ID
         )
         response = request.execute()
 
         #pprint.pprint(response)
         #st.write(response)
         channel_nm = response["items"][0]["snippet"]["title"]
-
-
 
         # Check if the channel name exists in the channel collection
         Get_Channel_Name = channels_collection.find_one({'Channel_Name.Channel_Name': channel_nm})
@@ -72,13 +76,12 @@ def extract_channel_data(Channel_ID):
                     )
                     pl_response = pl_request.execute()
 
-                    # Extract the video IDs from the response and add them to the chl_all_videos list
+                    # Extract the video IDs from the playlist response and add them to the chl_all_videos list
                     for i in pl_response["items"]:
                         video_id = i["contentDetails"]["videoId"]
                         chl_all_video_ids.append(video_id)
 
                     # Check in the given channel if there are more pages to read
-                    # If no more pages present then exit the loop
                     next_page_token = pl_response.get("nextPageToken")
                     if not next_page_token:
                         break  
@@ -87,8 +90,6 @@ def extract_channel_data(Channel_ID):
 
             ch_all_video_ids = fetch_all_video_ids(Playlist_ID)
 
-            #print(ch_all_video_ids)
-            
             channel_data = {
             "Channel_Name":{
             "Channel_Name": response["items"][0]["snippet"]["title"],
@@ -111,8 +112,6 @@ def extract_channel_data(Channel_ID):
                 )
                 vd_response = vd_request.execute()
                 
-                #print(i)
-
                 video_data = {"Video_Id_"+str(cnt1):{
                 "Video_Id": vd_response["items"][0]["id"],
                 "Video_Name": vd_response["items"][0]["snippet"]["title"],
@@ -120,7 +119,7 @@ def extract_channel_data(Channel_ID):
                 "Tags": vd_response["items"][0]["snippet"].get("tags", []),
                 "PublishedAt": vd_response["items"][0]["snippet"]["publishedAt"],
                 "View_Count": vd_response["items"][0]["statistics"]["viewCount"],
-                "Like_Count": vd_response["items"][0]["statistics"]["likeCount"],
+                "Like_Count": vd_response["items"][0]["statistics"].get("likeCount",0),
                 "Dislike_Count": "0",
                 "Favorite_Count": vd_response["items"][0]["statistics"]["favoriteCount"],
                 "Comment_Count": vd_response["items"][0]["statistics"].get("commentCount", 0),
@@ -139,9 +138,7 @@ def extract_channel_data(Channel_ID):
                         part="snippet,replies",
                         videoId=i
                     )
-                    #print(i)
                     cm_response = cm_request.execute()
-                    #print(cm_response)
                     cnt2 = 0
                     for comment_item in cm_response["items"]:
                         cnt2 += 1
@@ -155,94 +152,30 @@ def extract_channel_data(Channel_ID):
                         }
                         
                         channel_video_comment_dict[f"Video_Id_{cnt1}"]["Comments"].update(comment_data)
-                #else:
-                    #print(f"Comments are disabled for the video with ID {i}. Skipping comment retrieval.")
-                    
-            #print(channel_video_comment_dict)
-
-            #return channel_video_comment_dict
-            
-            client.close()
-            #st.write(channel_video_comment_dict)
             return channel_video_comment_dict, 1
         else:
-            client.close()
             return 0,0
+
     except Exception as err:
-        client.close()
-        st.write("An unexpected error occured: ", err)
-        traceback.print_exc()
+        st.error("An unexpected error occured: " + str(err))
         return None
     except googleapiclient.errors.HttpError as http_err:
-        client.close()
         error_message = json.loads(http_err.content)['error']['message']
-        st.write(f"An HTTP error occurred: {error_message}")
-        
+        st.error(f"An HTTP error occurred: {error_message}")
 
-#channel_video_comment_dictionary = extract_channel_data(chID)
-
-#def load_youtube_data_to_mongodb(v2):
-#    channels_collection.insert_one(v2) #channel_video_comment_dict)
-#    st.success("Channel data is inserted successfully into MongoDB")
-
-
-
-
-distinct_values = channels_collection.distinct("Channel_Name.Channel_Name")
-
-
-
+#Function to migrate from mongodb to mysql
 def migrate_channel_data_from_mongo_to_mysql(selected_chl):
 
     try:
 
-        uri1 = "mongodb://localhost:27017/"
+        channel_documents = channels_collection.find_one({"Channel_Name.Channel_Name": selected_chl})
 
-        client1 = pymongo.MongoClient(uri1)
-        db1 = client["youtubedb"]
-        channels_collection1 = db["channels"]
-
-        mysql_config = {
-        'user':'dev_user', 'password':'My$ql123',
-        'host':'127.0.0.1', 'database':'youtubedb'
-        }
-
-        mysql_connect = mysql.connector.connect(**mysql_config)
-
-        mysql_cursor = mysql_connect.cursor()
-        
-        channel_documents = channels_collection1.find_one({"Channel_Name.Channel_Name": selected_chl})
-
-        #st.write(channel_documents["Channel_Name"])
-        # Retrieve data from MongoDB collection
-        #channel_documents = 1 #channels_collection1({})
-
-        delete_sql_queries = [
-            "DELETE FROM youtubedb.comment;",
-            "DELETE FROM youtubedb.video;",
-            "DELETE FROM youtubedb.playlist;",
-            "DELETE FROM youtubedb.channel;"
-        ]
-
-        for query in delete_sql_queries:
-            mysql_cursor.execute(query)
-
-        #st.write(type(channel_documents))  
-        # Extract data from MongoDB document
-        #for channel_document in channel_documents:
-        #   st.write("1111")
-        #   st.write("222",channel_document,channel_documents["Channel_Name"]["Channel_Name"])
-        #channel_data = channel_document["Channel_Name"]["Channel_Name"]
         channel_id = channel_documents["Channel_Name"]["Channel_Id"]
         channel_name = channel_documents["Channel_Name"]["Channel_Name"]
         subscription_count = channel_documents["Channel_Name"]["Subscription_Count"]
         channel_views = channel_documents["Channel_Name"]["Channel_Views"]
         channel_description = channel_documents["Channel_Name"]["Channel_Description"]
         playlist_id = channel_documents["Channel_Name"]["Playlist_Id"]
-
-            #mysql_cursor.execute(
-            #"delete from youtubedb.video; delete from youtubedb.playlist; delete from youtubedb.channel;"
-            #)
 
         mysql_cursor.execute(
         "INSERT INTO channel (channel_id, channel_name, subscription_count, channel_views, channel_description) VALUES (%s, %s, %s, %s, %s)",
@@ -257,7 +190,6 @@ def migrate_channel_data_from_mongo_to_mysql(selected_chl):
         for key, value in channel_documents.items():
             if key.startswith("Video_Id"):
                 video_data = value
-                #print(video_data)
                 video_id = video_data.get("Video_Id", "")
                 video_name = video_data.get("Video_Name", "")
                 video_description = video_data.get("Video_Description", "")
@@ -297,49 +229,117 @@ def migrate_channel_data_from_mongo_to_mysql(selected_chl):
 
         mysql_connect.commit()
 
-        # Closing connections
-        client1.close()
-        mysql_cursor.close()
-        mysql_connect.close()
-
-        st.write("Selected Channel Data migrated to Mysql")
+        st.success("Selected Channel Data migrated to Mysql")
     except Exception as err:
         client.close()
+        st.error("An unexpected error occured: ", err)
+        traceback.print_exc()
+        return None 
+
+#Function to get available channels for migration
+def get_channels_to_migrate():
+    mongodb_channels = channels_collection.distinct("Channel_Name.Channel_Name")
+    df1 = pd.DataFrame(list(mongodb_channels),columns = ['channel_name'])
+    available_mysql_channel = "select channel_name from channel"
+    mysql_cursor.execute(available_mysql_channel)
+    mysql_channels = mysql_cursor.fetchall()
+    df2 = pd.DataFrame(mysql_channels, columns = mysql_cursor.column_names)
+    concatenated_df = pd.concat([df1, df2])
+    difference_df = concatenated_df.drop_duplicates(keep=False)
+    channels_for_migration = difference_df['channel_name'].tolist()
+    return channels_for_migration
+
+#Function to get stats from mysql channel data
+def youtube_stats(selected_qry):
+    try:
+        
+        if selected_qry == 1:
+
+            select1 = "select a.video_name 'Video Name',c.channel_name 'Channel Name' from video a join playlist b on a.playlist_id = b.playlist_id join channel c on b.channel_id = c.channel_id; "
+
+        if selected_qry == 2:
+
+            select1 = "select c.channel_name 'Channel Name', count(1) 'Video Count'  from video a join playlist b on a.playlist_id = b.playlist_id    join channel c on b.channel_id = c.channel_id   group by c.channel_name   order by count(1) desc;"
+
+        if selected_qry == 3:
+
+            select1 = "select a.video_name 'Video Name',c.channel_name 'Channel Name' from video a join playlist b on a.playlist_id = b.playlist_id join channel c on b.channel_id = c.channel_id order by view_count desc limit 10;"
+
+        if selected_qry == 4:
+
+            select1 = "select a.video_name 'Video Name', count(1) 'Comment Count' from video a join comment b on a.video_id = b.video_id group by video_name;"
+
+        if selected_qry == 5:
+
+            select1 = "select a.video_name 'Video Name',a.like_count 'Count',c.channel_name 'Channel Name' from video a join playlist b on a.playlist_id = b.playlist_id    join channel c on b.channel_id = c.channel_id;"
+        if selected_qry == 6:
+
+            select1 = "select video_name,like_count, dislike_count from video;"
+
+        if selected_qry == 7:
+
+            select1 = "select c.channel_name 'Channel Name', sum(view_count) 'Views' from video a join playlist b on a.playlist_id = b.playlist_id    join channel c on b.channel_id = c.channel_id group by channel_name;"
+
+        if selected_qry == 8:
+
+            select1 = "select a.channel_name 'Channel Name', year(c.published_date) 'Video Publised Year' from channel a join playlist b on a.channel_id = b.channel_id  join video c on b.playlist_id = c.playlist_id where year(c.published_date) = '2022' group by a.channel_name,year(c.published_date);"
+
+        if selected_qry == 9:
+
+            select1 = "select c.channel_name 'Channel Name', case when FLOOR(avg(a.duration) / 3600) = 0 then concat(FLOOR((avg(a.duration) % 3600) / 60), ' Minutes') else concat(FLOOR(avg(a.duration) / 3600) ,' Hour ',FLOOR((avg(a.duration) % 3600) / 60), ' Minutes') end as 'Average Duration' from video a join playlist b on a.playlist_id = b.playlist_id    join channel c on b.channel_id = c.channel_id group by c.channel_name;"
+
+        if selected_qry == 10:
+
+            select1 = "select a.video_name 'Video Name', c.channel_name 'Channel Name', a.comment_count 'Comment Count'  from video a join playlist b on a.playlist_id = b.playlist_id    join channel c on b.channel_id = c.channel_id order by a.comment_count desc;"
+
+        mysql_cursor.execute(select1)
+
+        result = mysql_cursor.fetchall()
+
+        df = pd.DataFrame(result, columns = mysql_cursor.column_names)
+
+        st.write(df)
+
+        mysql_cursor.close()
+
+        mysql_connect.close()
+    
+    except Exception as err:
         st.write("An unexpected error occured: ", err)
         traceback.print_exc()
         return None 
 
-def youtube_stats(selected_qry):
-    st.write(selected_qry,type(selected_qry))
-
-def page1():
-    #st.sidebar("st_test.py", label="Home", icon="🏠")
-    #st.sidebar.page_link("st_test.py", label="ETL", icon="🔧🔩🔨")
-    #st.sidebar.page_link("st_test.py", label="View", icon="👁️‍🗨️")
-    #"How would you like to be contacted?",
-    #("Email", "Home phone", "Mobile phone"))
+def main():
 
     st.title("YOUTUBE HARVEST")
 
+    chID = st.text_input("Enter Channel Id")
     if st.button("Extract Data"):
-        value1,rerutn_code1 = extract_channel_data(chID)
-        if rerutn_code1 == 0:
-            st.error("Channel data aleady available, please enter different Channel Id")
+        if chID is "":
+            st.error("Please enter Channel Id")
         else:
-            st.success("Channel data extracted successfully")
-        
-    if st.button("Upload to MongoDB"):
-        value2,return_code2 = extract_channel_data(chID)
-        if return_code2 == 0:
-            st.error("Channel data aleady available, please enter different Channel Id")
-        else:
-            channels_collection.insert_one(value2)
-            st.success("Channel data is inserted successfully into MongoDB")
+            value1,rerutn_code1 = extract_channel_data(chID)
+            if rerutn_code1 == 0:
+                st.error("Channel data aleady available, please enter different Channel Id")
+            else:
+                st.success("Channel data extracted successfully")
 
-    selected_channel = st.selectbox("Select a CHannel to begin Trasformation to SQL",distinct_values)
+    if st.button("Upload to MongoDB"):
+        if chID is "":
+            st.error("Please enter Channel Id")
+        else:
+            value2,return_code2 = extract_channel_data(chID)
+            if return_code2 == 0:
+                st.error("Channel data aleady available, please enter different Channel Id")
+            else:
+                channels_collection.insert_one(value2)
+                st.success("Channel data is inserted successfully into MongoDB")
+
+    selected_channel = st.selectbox("Select a Channel to begin Trasformation to SQL",get_channels_to_migrate())
 
     if st.button("Submit"):
         migrate_channel_data_from_mongo_to_mysql(selected_channel)
+        client.close()
 
     st.title("Select any questions to get Insights")
 
@@ -360,5 +360,4 @@ def page1():
     if selected_query:
         youtube_stats(int(selected_query.split(r".")[0]))
 
-
-page1()
+main()
